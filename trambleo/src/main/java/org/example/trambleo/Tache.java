850b9e9ab2a4f2c118d920e8519e8daa8f1972b1
integeek <ambre.giraudlanza@outlook.fr>
2024-12-27 13:12:50 +0100

    Class Tache et sousTache

--- trambleo/src/main/java/org/example/trambleo/Tache.java
+++ trambleo/src/main/java/org/example/trambleo/Tache.java
@@ -1,4 +1,97 @@
 package org.example.trambleo;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+
 public class Tache {
+    UUID idTache;
+    String nomTache;
+    String descriptionTache;
+    String commentaire;
+    String priorite;
+    Date dateFinTache;
+    Categorie categorie;
+    ArrayList<Employe> listeEmployeTache;
+
+    public Tache(UUID idTache, String nomTache, String descriptionTache, String commentaire, String priorite, Date dateFinTache, Categorie categorie, ArrayList<Employe> listeEmployeTache) {
+        this.idTache = idTache;
+        this.nomTache = nomTache;
+        this.descriptionTache = descriptionTache;
+        this.commentaire = commentaire;
+        this.priorite = priorite;
+        this.dateFinTache = dateFinTache;
+        this.categorie = categorie;
+        this.listeEmployeTache = listeEmployeTache;
+    }
+
+    public String toString() {
+        return "identifiant : " + idTache + ", nom : " + nomTache + ", description : " + descriptionTache + ", commentaire : " + commentaire + ", priorité : " + priorite + ", date de fin : " + dateFinTache + ", categorie : " + categorie;
+    }
+
+    // <editor-fold desc="Getter and setter">
+    public UUID getIdTache() {
+        return idTache;
+    }
+
+    public void setIdTache(UUID idTache) {
+        this.idTache = idTache;
+    }
+
+    public String getNomTache() {
+        return nomTache;
+    }
+
+    public void setNomTache(String nomTache) {
+        this.nomTache = nomTache;
+    }
+
+    public String getDescriptionTache() {
+        return descriptionTache;
+    }
+
+    public void setDescriptionTache(String descriptionTache) {
+        this.descriptionTache = descriptionTache;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+    public String getPriorite() {
+        return priorite;
+    }
+
+    public void setPriorite(String priorite) {
+        this.priorite = priorite;
+    }
+
+    public Date getDateFinTache() {
+        return dateFinTache;
+    }
+
+    public void setDateFinTache(Date dateFinTache) {
+        this.dateFinTache = dateFinTache;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public ArrayList<Employe> getListeEmployeTache() {
+        return listeEmployeTache;
+    }
+
+    public void setListeEmployeTache(ArrayList<Employe> listeEmployeTache) {
+        this.listeEmployeTache = listeEmployeTache;
+    }
+    // </editor-fold>
 }
--- trambleo/src/main/java/org/example/trambleo/sousTache.java
+++ trambleo/src/main/java/org/example/trambleo/sousTache.java
@@ -1,4 +1,55 @@
 package org.example.trambleo;
 
+import java.util.UUID;
+
 public class sousTache {
+    UUID idSousTache;
+    String nomSousTache;
+    String descriptionSousTache;
+    boolean isChecked;
+
+    public sousTache(UUID idSousTache, String nomSousTache, String descriptionSousTache, boolean isChecked) {
+        this.idSousTache = idSousTache;
+        this.nomSousTache = nomSousTache;
+        this.descriptionSousTache = descriptionSousTache;
+        this.isChecked = isChecked;
+    }
+
+    public String toString(){
+        return "identifiant : " + idSousTache + ", nom :" + nomSousTache + ", description :" + descriptionSousTache + ", est completé ?  :" + isChecked;
+    }
+
+    // <editor-fold desc="Getter and setter">
+    public UUID getIdSousTache() {
+        return idSousTache;
+    }
+
+    public void setIdSousTache(UUID idSousTache) {
+        this.idSousTache = idSousTache;
+    }
+
+    public String getNomSousTache() {
+        return nomSousTache;
+    }
+
+    public void setNomSousTache(String nomSousTache) {
+        this.nomSousTache = nomSousTache;
+    }
+
+    public String getDescriptionSousTache() {
+        return descriptionSousTache;
+    }
+
+    public void setDescriptionSousTache(String descriptionSousTache) {
+        this.descriptionSousTache = descriptionSousTache;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+    // </editor-fold>
 }
