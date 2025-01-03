701abd9d0b0131c5baa03959cf3697944532bd91
zeyluish <teogdn.pro@gmail.com>
2025-01-03 20:40:04 +0100

    Configuration de la class Categorie
    toString()
    Getter & Setter
    Constructeur

--- trambleo/src/main/java/org/example/trambleo/Categorie.java
+++ trambleo/src/main/java/org/example/trambleo/Categorie.java
@@ -1,4 +1,46 @@
 package org.example.trambleo;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Categorie {
+    UUID idCategorie = UUID.randomUUID();
+    String nomCategorie;
+    ArrayList<Tache> listeTache;
+
+    public Categorie(String nomCategorie) {
+        this.nomCategorie = nomCategorie;
+    }
+
+    // <editor-fold desc="Getters and setters">
+
+    public UUID getIdCategorie() {
+        return idCategorie;
+    }
+
+    public void setIdCategorie(UUID idCategorie) {
+        this.idCategorie = idCategorie;
+    }
+
+    public String getNomCategorie() {
+        return nomCategorie;
+    }
+
+    public void setNomCategorie(String nomCategorie) {
+        this.nomCategorie = nomCategorie;
+    }
+
+    public ArrayList<Tache> getListeTache() {
+        return listeTache;
+    }
+
+    public void setListeTache(ArrayList<Tache> listeTache) {
+        this.listeTache = listeTache;
+    }
+
+    // </editor-fold>
+
+    public String toString() {
+        return "Identifiant : " + idCategorie + "\nNom du categorie : " + nomCategorie + "\nListe de tache : " + listeTache;
+    }
 }
