4f9259954283b48c8e91ef47ab5673041d931fca
integeek <ambre.giraudlanza@outlook.fr>
2025-01-10 10:06:39 +0100

    NavBar + pageCompte

---
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerPageCompte.java
@@ -0,0 +1,12 @@
+package org.example.trambleo;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class HelloControllerPageCompte {
+
+}
