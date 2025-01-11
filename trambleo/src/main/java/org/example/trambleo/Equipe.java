fba46afa68b42212879dd6f7b00c8247f2179023
zeyluish <teogdn.pro@gmail.com>
2025-01-11 17:47:09 +0100

    Création Classe Equipe

---
+++ trambleo/src/main/java/org/example/trambleo/Equipe.java
@@ -0,0 +1,10 @@
+package org.example.trambleo;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Equipe {
+    UUID idEquipe;
+    ArrayList<Employe> listeMembre;
+    boolean isProjetAssocie;
+}
