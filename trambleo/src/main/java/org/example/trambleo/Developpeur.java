0d426a6710817c4acc43a83e75b28369dc630bc3
integeek <ambre.giraudlanza@outlook.fr>
2025-01-12 22:37:35 +0100

    créer classe developpeur

---
+++ trambleo/src/main/java/org/example/trambleo/Developpeur.java
@@ -0,0 +1,36 @@
+package org.example.trambleo;
+
+public class Developpeur extends Employe{
+    public Developpeur(String nom, String prenom, String email, String motDePasse, String poste) {
+        super(nom, prenom, email, motDePasse, poste);
+        this.poste = "developpeur";
+    }
+
+    public void creerSousTache(String nomSousTache){
+        sousTache sousTache = new sousTache(nomSousTache);
+        System.out.println("La sous tache a bien été créer");
+    }
+    public void terminerSousTache(sousTache sousTache){
+        sousTache.isChecked = true;
+        System.out.println("La sous tache a bien été terminé");
+
+    }
+    public void supprimerSousTache(sousTache sousTache){
+        sousTache.isSupprime = true;
+        System.out.println("La sous tache a été supprimée");
+
+    }
+    public void modifierSousTache(sousTache sousTache, String nouveauNom){
+        sousTache.nomSousTache = nouveauNom;
+        System.out.println("Le nom a bien été modifié");
+
+    }
+    public void modifierPrioriteTache(Tache tache, String nouvellePriorite){
+        tache.priorite = nouvellePriorite;
+        System.out.println("La priorité a bien été changé");
+    }
+    public void ajouterCommentaire(String commentaire, Tache tache){
+        tache.listeCommentaire.add(commentaire);
+        System.out.println("Le commentaire a bien été ajouté");
+    }
+}
--- trambleo/src/main/java/org/example/trambleo/HelloControllerNavbar.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerNavbar.java
@@ -13,16 +13,16 @@ import java.io.IOException;
 
 public class HelloControllerNavbar {
     @FXML
-    public void onCompteButtonClick(MouseEvent event) throws IOException {
+    public void onCompteButtonClick(MouseEvent event) throws IOException { //Ouvrir la page du compte
         FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-pageCompte.fxml"));
         VBox pageCompte = loader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Récupérer la fenetre actuelle.
         stage.setTitle("Mon Compte");
         stage.setScene(new Scene(pageCompte));
         stage.show();
     }
 
-    public void onProjetButtonClick(MouseEvent event) throws IOException {
+    public void onProjetButtonClick(MouseEvent event) throws IOException { //Ouvrir la page des projets
         FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-mesProjets.fxml"));
         VBox pageProjet = loader.load();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
