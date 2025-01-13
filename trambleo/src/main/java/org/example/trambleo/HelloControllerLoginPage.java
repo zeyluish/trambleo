04e747e4dcfaa0ac95b492c74c33c1b4c6792255
zeyluish <teogdn.pro@gmail.com>
2025-01-13 20:07:05 +0100

    boutons signuppage & loginpage
    seulement ceux pour passer de l'une à l'autre

--- trambleo/src/main/java/org/example/trambleo/HelloControllerLoginPage.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerLoginPage.java
@@ -1,14 +1,26 @@
 package org.example.trambleo;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloControllerLoginPage {
     @FXML
-    private Label welcomeText;
+    public void onBoutonCreer1(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-signuppage.fxml"));
+        VBox pageCreation = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Récupérer la fenetre actuelle.
+        stage.setTitle("Créer un compte");
+        stage.setScene(new Scene(pageCreation));
+        stage.show();
+    }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onBoutonConnecter1(MouseEvent event) throws IOException {
+        System.out.println("Se connecter");
     }
 }
--- trambleo/src/main/java/org/example/trambleo/HelloControllerSignUp.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerSignUp.java
@@ -1,14 +1,26 @@
 package org.example.trambleo;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloControllerSignUp {
     @FXML
-    private Label welcomeText;
+    public void onBoutonConnecter(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-loginpage.fxml"));
+        VBox pageConnexion = loader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Récupérer la fenetre actuelle.
+        stage.setTitle("Se connecter");
+        stage.setScene(new Scene(pageConnexion));
+        stage.show();
+        }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onBoutonCreer(MouseEvent event) throws IOException {
+        System.out.println("Creer compte");
     }
 }
