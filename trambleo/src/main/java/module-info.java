455d8f386ac0db242705042c4ea11f624adfc971
integeek <ambre.giraudlanza@outlook.fr>
2025-01-13 11:54:20 +0100

    implementation firebase

--- trambleo/src/main/java/module-info.java
+++ trambleo/src/main/java/module-info.java
@@ -5,6 +5,12 @@ module org.example.trambleo {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires java.desktop;
+    requires google.cloud.core;
+    requires com.google.auth.oauth2;
+    requires com.google.auth;
+    requires firebase.admin;
+    requires google.cloud.firestore;
+    requires com.google.api.apicommon;
 
     opens org.example.trambleo to javafx.fxml;
     exports org.example.trambleo;
