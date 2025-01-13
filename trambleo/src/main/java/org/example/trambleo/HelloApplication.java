db8a73536784bf18cf61d161a4df8a9d162ac809
zeyluish <teogdn.pro@gmail.com>
2025-01-14 00:29:54 +0100

    createTask

--- trambleo/src/main/java/org/example/trambleo/HelloApplication.java
+++ trambleo/src/main/java/org/example/trambleo/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-pageCompte.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-createtask.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Se connecter");
         stage.setScene(scene);
--- trambleo/src/main/java/org/example/trambleo/HelloControllerCreateTask.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerCreateTask.java
@@ -1,14 +1,101 @@
 package org.example.trambleo;
 
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class HelloControllerCreateTask {
-    @FXML
-    private Label welcomeText;
+
+    ArrayList<Employe> employeChoisi =  new ArrayList<>();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TextField nomTache;
+
+    @FXML
+    TextField descTache;
+
+    @FXML
+    RadioButton radFaible;
+
+    @FXML
+    RadioButton radMoyenne;
+
+    @FXML
+    RadioButton radEleve;
+
+    @FXML
+    DatePicker dateFin;
+
+    @FXML
+    ComboBox<Employe> comboEmploye;
+
+    @FXML
+    Label messageConfirmation;
+
+    @FXML
+    public void initialize() {
+        Employe.importEmploye();
+        comboEmploye.getItems().addAll(Employe.listeEmploye);
+    }
+
+    @FXML
+    public void onAttribuerEmployeBouton() throws IOException {
+        Employe employeSelectionne = comboEmploye.getSelectionModel().getSelectedItem();
+        if (employeSelectionne == null) {
+            messageConfirmation.setText("Veuillez sélectionner un employé.");
+            return;
+        }
+        if (employeChoisi.contains(employeSelectionne)) {
+            messageConfirmation.setText("L'employé " + employeSelectionne.prenom + " " + employeSelectionne.nom + " est déjà dans la liste.");
+        } else {
+            employeChoisi.add(employeSelectionne);
+            messageConfirmation.setText("L'employé " + employeSelectionne.prenom + " " + employeSelectionne.nom + " a bien été ajouté.");
+        }
+    }
+
+    String prioriteTache;
+
+    @FXML
+    public void onFaible() throws IOException {
+        String prioriteTache = "Faible";
+    }
+
+    @FXML
+    public void onMoyenne() throws IOException {
+        String prioriteTache = "Moyenne";
+    }
+
+    @FXML
+    public void onEleve() throws IOException {
+        String prioriteTache = "Eleve";
+    }
+
+    //^^^^^^^^^^^^^^^^^^//
+    //Essayer de voir si c'est possible de selectionner d'un radiobutton
+
+    @FXML
+    public void onCreerTacheBouton() throws IOException {
+        /*String nomTacheText = nomTache.getText();
+        String descriptionTacheText = descTache.getText();
+        LocalDate dateFinTacheDate = dateFin.getValue();
+        Projet nouvelleTache = ChefProjet.creerTache(nomProjet,nomTacheText,descriptionTacheText,prioriteTache,dateFinTacheDate);
+        for (Employe employe : employeChoisi) {
+            nouvelleTache.listeEmploye.add(employe);
+        }
+        System.out.println(nouvelleTache);*/
+        messageConfirmation.setText("La tâche a bien été créée");
+    }
+
+    //^^^^^^^^^^^^^^^^^^^//
+    // Faire en sorte que le nomProjet soit défini et ça devrait marcher
+
+    public void onAnnulerTacheBouton(){ //Fermer la pop up
+        Stage stage = (Stage) dateFin.getScene().getWindow();
+        stage.close();
     }
 }
