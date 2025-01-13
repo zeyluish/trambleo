cc7aae2b0b470caca771d715c477c194bd586136
zeyluish <teogdn.pro@gmail.com>
2025-01-13 23:17:24 +0100

    changement code affichage de la page pageCompte

--- trambleo/src/main/java/org/example/trambleo/HelloControllerPageCompte.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerPageCompte.java
@@ -3,9 +3,9 @@ package org.example.trambleo;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,21 +13,23 @@ import java.io.IOException;
 
 public class HelloControllerPageCompte {
     @FXML
-    public void onEditEmailBouton(MouseEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("editEmailPopUp.fxml"));
-        VBox pageEditEmail = loader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Récupérer la fenetre actuelle.
-        stage.setTitle("Modifier Email");
-        stage.setScene(new Scene(pageEditEmail));
+    public void onEditEmailBouton() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation((this.getClass().getResource("editEmailPopUp.fxml")));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
         stage.show();
     }
     @FXML
-    public void onEditPasswordBouton(MouseEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("editPasswordPopUp.fxml"));
-        VBox pageEditPassword = loader.load();
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Récupérer la fenetre actuelle.
-        stage.setTitle("Modifier Mot de passe");
-        stage.setScene(new Scene(pageEditPassword));
+    public void onEditPasswordBouton() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation((this.getClass().getResource("editPasswordPopUp.fxml")));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
         stage.show();
     }
+
+
 }
