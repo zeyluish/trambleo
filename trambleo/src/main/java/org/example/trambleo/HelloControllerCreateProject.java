a05643fe9977a4363305f2af5635ec9676f5664c
integeek <ambre.giraudlanza@outlook.fr>
2025-01-13 20:33:57 +0100

    Associe employé à un projet

--- trambleo/src/main/java/org/example/trambleo/HelloControllerCreateProject.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerCreateProject.java
@@ -3,14 +3,17 @@ package org.example.trambleo;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class HelloControllerCreateProject {
 
+    ArrayList<Employe> employeChoisi =  new ArrayList<>();
     @FXML
     TextField nomProjet;
     @FXML
@@ -21,6 +24,8 @@ public class HelloControllerCreateProject {
     DatePicker dateFinProjet;
     @FXML
     ComboBox<Employe> choixEmploye;
+    @FXML
+    Label messageConfirmation;
 
 
     @FXML
@@ -36,13 +41,20 @@ public class HelloControllerCreateProject {
         LocalDate dateDebutProjetDate = dateDebutProjet.getValue();
         LocalDate dateFinProjetDate = dateFinProjet.getValue();
         Projet nouveauProjet = Administrateur.creerProjet(nomProjetText,descriptionProjetText,dateDebutProjetDate,dateFinProjetDate);//Appel de la fonction pour créer un projet
+        for (Employe employe : employeChoisi) {
+            nouveauProjet.listeEmploye.add(employe);
+        }
         System.out.println(nouveauProjet);
+        messageConfirmation.setText("Le projet a bien été créé");
     }
 
     @FXML
     public void onAssocierEmployeButtonClick(){
-
+        Employe employeSelectionne = choixEmploye.getSelectionModel().getSelectedItem();
+        employeChoisi.add(employeSelectionne);
+        messageConfirmation.setText("L'employé " + employeSelectionne.prenom + " " + employeSelectionne.nom + " a bien été ajouté");
     }
+
     public void onAnnulerButtonClick(){ //Fermer la pop up
         Stage stage = (Stage) nomProjet.getScene().getWindow();
         stage.close();
