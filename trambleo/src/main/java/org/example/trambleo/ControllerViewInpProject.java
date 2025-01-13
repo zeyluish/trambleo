2f3be3b220a89ea1ef0078c0d424e6f93c9d4079
integeek <ambre.giraudlanza@outlook.fr>
2025-01-14 00:00:01 +0100

    pop up modifier Projet

--- trambleo/src/main/java/org/example/trambleo/ControllerViewInpProject.java
+++ trambleo/src/main/java/org/example/trambleo/ControllerViewInpProject.java
@@ -1,8 +1,12 @@
 package org.example.trambleo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class ControllerViewInpProject {
@@ -16,4 +20,14 @@ public class ControllerViewInpProject {
         String dateFinProjet2 = dateFin.toString();
         dateFinProjet.setText(dateFinProjet2);
     }
+
+    public void onModifierProjetButtonClick() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation((this.getClass().getResource("hello-editProject.fxml")));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
--- trambleo/src/main/java/org/example/trambleo/HelloControllerEditProject.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerEditProject.java
@@ -1,4 +1,29 @@
 package org.example.trambleo;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HelloControllerEditProject {
+    @FXML
+    TextField nomProjet;
+    @FXML
+    TextField descriptionProjet;
+    @FXML
+    DatePicker dateFinProjet;
+    @FXML
+    CheckBox statutEnCours;
+    @FXML
+    CheckBox statutEnRetard;
+    @FXML
+    CheckBox statutTermine;
+
+    public void onAnnulerButtonClick(){
+        Stage stage = (Stage) nomProjet.getScene().getWindow();
+        stage.close();
+    }
 }
