07328119280bc7e40a96c00507ff59b6f65e4ade
zeyluish <teogdn.pro@gmail.com>
2025-01-13 19:02:00 +0100

    debut controller editEmail

--- trambleo/src/main/java/org/example/trambleo/ControllerEditEmailPopUp.java
+++ trambleo/src/main/java/org/example/trambleo/ControllerEditEmailPopUp.java
@@ -1,4 +1,23 @@
 package org.example.trambleo;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class ControllerEditEmailPopUp {
+
+    @FXML
+    TextField ancienEmail;
+    @FXML
+    TextField nouveauEmail;
+    @FXML
+    TextField confirmerEmail;
+
+
+    public void onModifierEmailBoutonClick() throws IOException {
+        String ancienEmailText = ancienEmail.getText();
+        String nouveauEmailText = nouveauEmail.getText();
+
+    }
 }
