575bd325ff4cec4efa2113fd1f687a50632fa428
zeyluish <teogdn.pro@gmail.com>
2025-01-13 23:31:19 +0100

    ajout bouton annuler editPasswordPopUp

--- trambleo/src/main/java/org/example/trambleo/ControllerEditPasswordPopUp.java
+++ trambleo/src/main/java/org/example/trambleo/ControllerEditPasswordPopUp.java
@@ -1,4 +1,28 @@
 package org.example.trambleo;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class ControllerEditPasswordPopUp {
+
+    @FXML
+    TextField ancienPassword;
+    @FXML
+    TextField nouveauPassword;
+    @FXML
+    TextField confirmerPassword;
+
+    @FXML
+    public void onModifierPasswordBouton() throws IOException {
+        System.out.println("valider");
+    }
+
+    @FXML
+    public void onAnnulerBoutonClick(){
+        Stage stage = (Stage) ancienPassword.getScene().getWindow();
+        stage.close();
+    }
 }
