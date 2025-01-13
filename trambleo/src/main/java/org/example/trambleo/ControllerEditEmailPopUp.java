e6d4b6cde1745ebc0958326c23f1c57ece17d4ff
zeyluish <teogdn.pro@gmail.com>
2025-01-13 23:08:40 +0100

    bouton annuler sur editEmail

--- trambleo/src/main/java/org/example/trambleo/ControllerEditEmailPopUp.java
+++ trambleo/src/main/java/org/example/trambleo/ControllerEditEmailPopUp.java
@@ -2,23 +2,27 @@ package org.example.trambleo;
 
 import javafx.fxml.FXML;
 
-import java.awt.*;
 import java.io.IOException;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ControllerEditEmailPopUp {
 
     @FXML
     TextField ancienEmail;
+
     @FXML
     TextField nouveauEmail;
+
     @FXML
     TextField confirmerEmail;
 
-
     public void onModifierEmailBoutonClick() throws IOException {
-        String ancienEmailText = ancienEmail.getText();
-        String nouveauEmailText = nouveauEmail.getText();
-        String confirmerEmailText = confirmerEmail.getText();
 
+    }
+
+    public void onAnnulerBoutonClick(){
+        Stage stage = (Stage) ancienEmail.getScene().getWindow();
+        stage.close();
     }
 }
