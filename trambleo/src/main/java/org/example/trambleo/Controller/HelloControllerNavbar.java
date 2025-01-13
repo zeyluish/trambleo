7ec002b3d2695a17476c683392c7d87ac13c294c
integeek <ambre.giraudlanza@outlook.fr>
2025-01-13 12:36:28 +0100

    Revert "résoudre problème navbar"
    This reverts commit a18f48daad552fb227969d960d600cb19749255d.

--- trambleo/src/main/java/org/example/trambleo/Controller/HelloControllerNavbar.java
+++ trambleo/src/main/java/org/example/trambleo/Controller/HelloControllerNavbar.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class HelloControllerNavbar {
     @FXML
     public void onCompteButtonClick(MouseEvent event) throws IOException { //Ouvrir la page du compte
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/trambleo/hello-pageCompte.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-pageCompte.fxml"));
         VBox pageCompte = loader.load();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); //Récupérer la fenetre actuelle.
         stage.setTitle("Mon Compte");
@@ -22,7 +22,7 @@ public class HelloControllerNavbar {
     }
 
     public void onProjetButtonClick(MouseEvent event) throws IOException { //Ouvrir la page des projets
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/trambleo/hello-mesProjets.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-mesProjets.fxml"));
         VBox pageProjet = loader.load();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setTitle("Mon Compte");
