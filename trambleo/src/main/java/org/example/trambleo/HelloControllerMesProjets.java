02480e44bd09a040478fcf7ae5151c0de80e403e
integeek <ambre.giraudlanza@outlook.fr>
2025-01-13 12:46:33 +0100

    pop up creer un projet

--- trambleo/src/main/java/org/example/trambleo/HelloControllerMesProjets.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerMesProjets.java
@@ -1,4 +1,21 @@
 package org.example.trambleo;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HelloControllerMesProjets {
+    public void onNouveauProjetButtonClick() throws IOException { //Ouvrir la page du compte
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation((this.getClass().getResource("hello-createProject.fxml")));
+        Scene scene = new Scene(loader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
