f8f8136652b67eab99b3809041dd67230615a101
integeek <ambre.giraudlanza@outlook.fr>
2025-01-15 19:58:27 +0100

    password

--- trambleo/src/main/java/org/example/trambleo/HelloControllerLoginPage.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerLoginPage.java
@@ -6,6 +6,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
@@ -20,7 +21,7 @@ public class HelloControllerLoginPage {
     @FXML
     TextField emailLogin;
     @FXML
-    TextField passwordLogin;
+    PasswordField passwordLogin;
     @FXML
     Label messageConfirmation;
 
