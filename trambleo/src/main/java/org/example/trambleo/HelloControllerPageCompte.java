942688dfa167fb398b3b655dd60df572d9bac88f
integeek <ambre.giraudlanza@outlook.fr>
2025-01-16 23:04:24 +0100

    final

--- trambleo/src/main/java/org/example/trambleo/HelloControllerPageCompte.java
+++ trambleo/src/main/java/org/example/trambleo/HelloControllerPageCompte.java
@@ -92,25 +92,4 @@ public class HelloControllerPageCompte {
             }
         }
     }
-
-    @FXML
-    public void onEditEmailBouton() throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation((this.getClass().getResource("editEmailPopUp.fxml")));
-        Scene scene = new Scene(loader.load());
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
-    }
-    @FXML
-    public void onEditPasswordBouton() throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation((this.getClass().getResource("editPasswordPopUp.fxml")));
-        Scene scene = new Scene(loader.load());
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
-    }
-
-
 }
